62ceced6c222b4d3fd912351ec7a456c684e4ff8
Julius Sommer <j.sommer.2@tu-berlin.de>
2024-01-27 00:06:47 +0100

    Added Figure 6

--- flim_ui.py
+++ flim_ui.py
@@ -1,36 +1,39 @@
+import matplotlib.font_manager
 import matplotlib.pyplot as plt
 
 main_params = {
     # text
+    "font.family": "sans-serif",
+    "font.sans-serif": ["Arial"],
     "font.size": 24,
+    "pdf.fonttype": 42,
     # axes
     "axes.linewidth": 2,
     # "axes.edgecolor" : "grey",
     "axes.titlesize": 20,
     "axes.labelsize": 20,
     # lines
-    "lines.linewidth": 5,
+    "lines.linewidth": 2,
     # scatter
-    "lines.linewidth": 5,
-    "lines.markersize": 14,
+    "lines.linewidth": 2,
+    "lines.markersize": 30,
     # x-axis
     "xtick.top": True,
     "xtick.labelsize": 20,
     "xtick.major.size": 9,
     "xtick.minor.size": 6,
-    "xtick.major.width": 1.4,
+    "xtick.major.width": 2,
     "xtick.minor.width": 1.4,
     "xtick.major.pad": 6,
     "xtick.minor.pad": 6,
     "xtick.direction": "in",
     "xtick.minor.visible": True,
-    # "axes.ymargin" : 0,
     # y-axis
     "ytick.right": True,
     "ytick.labelsize": 20,
     "ytick.major.size": 9,
     "ytick.minor.size": 6,
-    "ytick.major.width": 1.4,
+    "ytick.major.width": 2,
     "ytick.minor.width": 1.4,
     "ytick.major.pad": 6,
     "ytick.minor.pad": 6,
@@ -42,7 +45,7 @@ main_params = {
     "grid.alpha": 0.3,
     "grid.linewidth": 0.5,
     # figure
-    "figure.figsize": (6, 6),
+    "figure.figsize": (8.25, 5),
     # savefig
     "savefig.bbox": "tight",
     "savefig.dpi": 200,
@@ -52,7 +55,7 @@ main_params = {
 
 fullwidth_params = {
     # figure
-    "figure.figsize": (16, 6),
+    "figure.figsize": (17.8, 5),
 }
 
 PARAMS = {"main": main_params, "fullwidth": fullwidth_params}
